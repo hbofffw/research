7ebd653759b04e0f08bed332a9cbd0e5fb7ef06a
unlandable <unlandable@me.com>
2015-11-30 16:17:52 +0800

    pc

--- matlab/gas2dimensional.m
+++ matlab/gas2dimensional.m
@@ -10,10 +10,10 @@ C1 = (M./(2*sqrt((D*pi).*t))).*exp(-(x1-u*t).^2./((D*4).*t));
 C2 = (M./(2*sqrt((D*pi).*t))).*exp(-(x2-u*t).^2./((D*4).*t));
 C3 = (M./(2*sqrt((D*pi).*t))).*exp(-(x3-u*t).^2./((D*4).*t));
 C4 = (M./(2*sqrt((D*pi).*t))).*exp(-(x4-u*t).^2./((D*4).*t));
-subplot(4,1,1); plot(t,C1);title([x1]);
-subplot(4,1,2); plot(t,C2);title('x=20');
-subplot(4,1,3); plot(t,C3);title('concentration');
-subplot(4,1,4); plot(t,C4);title('concentration');
+subplot(4,1,1); plot(t,C1);title(['x=',num2str(x1)]);
+subplot(4,1,2); plot(t,C2);title(['x=',num2str(x2)]);
+subplot(4,1,3); plot(t,C3);title(['x=',num2str(x3)]);
+subplot(4,1,4); plot(t,C4);title(['x=',num2str(x4)]);
 
 fid=fopen('C1.txt','wt');
 fprintf(fid,'%g, ',C1);
